e76e4bf751816a9ce33d2cbac5cafb5240b59342
KimYuah <www0157@naver.com>
2020-09-09 17:57:01 +0900

    ㅇㅇ

--- src/main/java/mini/project/handler/BookHandler.java
+++ src/main/java/mini/project/handler/BookHandler.java
@@ -1,7 +1,6 @@
 package mini.project.handler;
 
 import java.sql.Date;
-import com.eomcs.pms.domain.Board;
 import mini.project.domain.Book;
 import mini.project.util.Iterator;
 import mini.project.util.List;
@@ -50,8 +49,7 @@ public class BookHandler {
           book.getTitle(),
           book.getAuthor(),
           book.getPublisher(),
-          book.getReceivingDate(),
-          book.getViewCount());
+          book.getReceivingDate());
     }
   }
 
