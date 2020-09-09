d6eb717434cfaa205ac1e1d0b2ce891d67398a43
sejunO <frame8916@gmail.com>
2020-09-09 14:53:42 +0900

    .

---
+++ src/main/java/mini/project/BookHandler.java
@@ -0,0 +1,17 @@
+package mini.project;
+
+public class BookHandler {
+  BookInfo bookInfo;
+
+  public BookHandler(BookInfo bookInfo) {
+    this.bookInfo = bookInfo;
+  }
+
+  public void borrowBook() {
+  }
+
+  public void checkBook() {
+  }
+
+
+}
